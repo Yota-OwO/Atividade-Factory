a09d498bf570d250f4488c05cf8a6aab7f398184
Yota-OwO <yotinhaa@gmail.com>
2026-03-21 18:52:24 -0300

    modified:   personagem.java

--- personagem.java
+++ personagem.java
@@ -5,3 +5,38 @@ class PersonagemBuilder{
     private int armadura;
     private String habilidades;
 }
+
+public personagemBuilder comNome(String n) {
+    this.nome = n;
+    return this;
+}
+public personagemBuilder comClasse(String c){
+    this.classe = c;
+    return this;
+}
+public personagemBuilder comArma(String a){
+    this.arma = a;
+    return this;
+}
+public personagemBuilder comArmadura(int ar){
+    this.armadura = ar;
+    return this;
+}
+public personagemBuilder comHabilidades(String h){
+    this.habilidades = h;
+    return this;
+}
+public class main {
+    public static void main(String[] args) {
+        
+        Personagem personagem = new PersonagemBuilder()
+        .comNome("Aragorn")
+        .comClasse("Guerreiro")
+        .comArma("Espada")
+        .comArmadura(100)
+        .comHabilidades("Liderança, Combate corpo a corpo")
+        .build();
+
+        personagem.exibirDetalhes();
+    }
+}
